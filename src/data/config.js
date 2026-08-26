9669d5dd9e4f2b3f030248fe4998c92fcca1ec3d
Student Developer <student@kvrewari.edu>
2026-08-26 15:49:57 +0530

    Update Daily Quiz Google Drive link

--- src/data/config.js
+++ src/data/config.js
@@ -15,7 +15,7 @@
 
 export const resourceLinks = {
     // Core Resource Hub Links
-    dailyQuiz: "https://drive.google.com/drive/folders/1Z-tWMvfyVBRNYTXz3jyaT238hkviQbwQ?usp=drive_link", // 🔥 Daily quiz Google Drive link
+    dailyQuiz: "https://drive.google.com/file/d/1KssOrYfMq4mmpj1Af2SavUHfX7o56t8M/view?usp=sharing", // 🔥 Daily quiz Google Drive link
     lectures: "",            // 🎥 Topic-wise recorded lectures
     notes: "",               // 📚 Class notes, revision notes and PDFs
     animatedNotes: "",       // ⚡ Animated visual short notes
